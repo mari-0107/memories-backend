fe24bdffe456847323a6245e0504ba8d47f57ec2
Maria <m.krivetz@gmail.com>
2023-10-18 16:03:50 +0300

    app

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use("/posts", PostsRoutes)
 
-application.get("/", (req, res) => {res.send("Hello to API")})
+app.get("/", (req, res) => {res.send("Hello to API")})
 
 // const CONNECTION_URL = 'mongodb+srv://Maria:Maria123@cluster0.y9los7j.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
