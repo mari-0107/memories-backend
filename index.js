1251eceb3d052f060f35114dfc4d02b9b9b8409d
Maria <m.krivetz@gmail.com>
2023-10-19 19:07:03 +0300

    remove useupdate

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, ()=> console.log(`running on ${PORT}`))).catch((error)=> console.log(error.message))
 
-mongoose.set('useFindAndModify', false)
+// mongoose.set('useFindAndModify', false)
 
 
 
