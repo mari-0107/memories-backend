f09fed12b76841b420f5eabba58ca18cd8949f00
Maria <m.krivetz@gmail.com>
2023-10-19 19:23:04 +0300

    router

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 
-import PostsRoutes from './routes/posts.js'
+import router from './routes/posts.js'
 
 const app = express();
 dotenv.config()
@@ -13,8 +13,8 @@ dotenv.config()
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
-app.use("/posts", PostsRoutes)
-
+// app.use("/posts", PostsRoutes)
+app.use("/posts", router)
 app.get("/", (req, res) => {res.send("Hello to API")})
 
 const CONNECTION_URL = 'mongodb+srv://Maria:Maria123@cluster0.y9los7j.mongodb.net/?retryWrites=true&w=majority'
--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 
-const router = express.Router();
+const router = express.Router()
 
 router.get("/", getPosts);
 router.post("/", createPost);
